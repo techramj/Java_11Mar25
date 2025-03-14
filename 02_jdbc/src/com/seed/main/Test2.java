ad43bdefbad138f83d48935829c1a6ad0a7e9c56
techramj <79793143+techramj@users.noreply.github.com>
2025-03-14 20:57:46 +0530

    meta data

--- 02_jdbc/src/com/seed/main/Test2.java
+++ 02_jdbc/src/com/seed/main/Test2.java
@@ -1,35 +1,66 @@
 package com.seed.main;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.seed.entity.Employee;
 import com.seed.util.ConnectionUtil;
 
 public class Test2 {
-	
+
 	public static void main(String[] args) {
-		selectExample();
-	}
-	
-	
-	public static void selectExample() {
-		String sql ="select * from emp_11mar";
+		//selectExample();
+		List<Employee> empList = getEmployeeSalaryGreaterThan(10000);
+		empList.forEach(e->  System.out.println(e));
 		
-		try(Statement st = ConnectionUtil.getConnection().createStatement()){
-			
-			try(ResultSet rs = st.executeQuery(sql)){
-				while(rs.next()) {
-					int id = rs.getInt(1);  //rs.getInt("id");
+		System.out.println("-----------------");
+		getEmployeeSalaryGreaterThan(5000).forEach(e->  System.out.println(e));
+	}
+
+	public static List<Employee> getEmployeeSalaryGreaterThan(double salary) {
+		String sql = "select * from emp_11mar where salary > ?";
+		List<Employee> empList = new ArrayList<>();
+
+		try (PreparedStatement ps = ConnectionUtil.getConnection().prepareStatement(sql)) {
+			ps.setDouble(1, salary);
+			try (ResultSet rs = ps.executeQuery()) {
+				while (rs.next()) {
+					int id = rs.getInt(1); // rs.getInt("id");
 					String name = rs.getString(2);
-					double salary = rs.getDouble(3);
-					System.out.println(id+"   "+name+"   "+salary);
+					double empSal = rs.getDouble(3);
+					// System.out.println(id + " " + name + " " + salary);
+					Employee emp = new Employee(id, name, empSal);
+					empList.add(emp);
 				}
 			}
-			
-		}catch(Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+		return empList;
+	}
+
+	public static void selectExample() {
+		String sql = "select * from emp_11mar";
+
+		try (Statement st = ConnectionUtil.getConnection().createStatement()) {
+
+			try (ResultSet rs = st.executeQuery(sql)) {
+				while (rs.next()) {
+					int id = rs.getInt(1); // rs.getInt("id");
+					String name = rs.getString(2);
+					double salary = rs.getDouble(3);
+					System.out.println(id + "   " + name + "   " + salary);
+				}
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
---
+++ 02_jdbc/src/com/seed/main/Test3.java
@@ -0,0 +1,103 @@
+package com.seed.main;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import com.seed.util.ConnectionUtil;
+
+public class Test3 {
+	
+	
+	public static void main(String[] args) {
+		
+		try {
+			dbMetaData();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//String sql1 = "update emp_11mar set salary = salary + 1000";
+		//String sql2 = "delete from emp_11mar where salary<8000";
+		String sql3 = "select * from emp_11mar";
+		String sql4 = "select * from dept2";
+		
+		//executeMethodExample(sql1);
+		executeMethodExample1(sql3);
+		executeMethodExample1(sql4);
+		executeMethodExample1("select * from emp2");
+	}
+	
+	
+	public static void executeMethodExample1(String sql) {
+		Connection con = ConnectionUtil.getConnection();
+		try(Statement st = con.createStatement()){
+			if(st.execute(sql)) {
+				try(ResultSet rs = st.getResultSet()){
+					//no of column, type of column
+					 ResultSetMetaData rsmd = rs.getMetaData();
+					 int colCount = rsmd.getColumnCount();
+					 for(int i=1;i<=colCount;i++) {
+						 String colName = rsmd.getColumnName(i); 
+						 System.out.print(colName+"\t\t\t");
+					 }
+					 System.out.println();
+					 
+					 while(rs.next()) {
+						 for(int i=1;i<=colCount;i++) {
+							 System.out.print(rs.getObject(i)+"\t\t\t");
+						 } 
+						 System.out.println();
+					 }
+				}
+				
+			}else {
+				//insert, update, delete
+				int count = st.getUpdateCount();
+				System.out.println(count+" row(s) affected!!!");
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("=========================================");
+	}
+	
+	public static void executeMethodExample(String sql) {
+		Connection con = ConnectionUtil.getConnection();
+		try(Statement st = con.createStatement()){
+			if(st.execute(sql)) {
+				try(ResultSet rs = st.getResultSet()){
+					while (rs.next()) {
+						int id = rs.getInt(1); // rs.getInt("id");
+						String name = rs.getString(2);
+						double salary = rs.getDouble(3);
+						System.out.println(id + "   " + name + "   " + salary);
+					}
+				}
+				
+			}else {
+				//insert, update, delete
+				int count = st.getUpdateCount();
+				System.out.println(count+" row(s) affected!!!");
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void dbMetaData() throws SQLException {
+		Connection con = ConnectionUtil.getConnection();
+		DatabaseMetaData md = con.getMetaData();
+		
+		System.out.println("Product name: " +md.getDatabaseProductName());
+		
+		System.out.println("max col in select: " +md.getMaxColumnsInSelect());
+		System.out.println("max col in table: "+md.getMaxColumnsInTable());
+
+	}
+	
+
+}
