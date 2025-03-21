9a4d8a33695c9bfbe5e03ffd78d92f07f6d5008d
techramj <79793143+techramj@users.noreply.github.com>
2025-03-21 20:26:16 +0530

    config and context

--- 03_wcd1/src/main/java/com/seed/servlet/LifecycleServlet.java
+++ 03_wcd1/src/main/java/com/seed/servlet/LifecycleServlet.java
@@ -3,6 +3,7 @@ package com.seed.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,11 +11,28 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet(name="lifcycle", urlPatterns ="/lifecycle" , loadOnStartup = 1 )
+//@WebServlet(name="lifcycle", urlPatterns ="/lifecycle" , loadOnStartup = 1 )
 public class LifecycleServlet extends HttpServlet {
 	
 	public LifecycleServlet() {
 		System.out.println(this.getClass().getSimpleName()+" construtor called");
+	}
+	
+	@Override
+	public void init() throws ServletException {
+		
+		//read the init param value
+		String a = this.getInitParameter("a");
+		System.out.println("a="+a);
+		
+		//read the context param
+		ServletContext context = this.getServletContext();
+		String driverName = context.getInitParameter("");
+		String url = context.getInitParameter("");
+		String username = context.getInitParameter("db.username");
+		String password = context.getInitParameter("db.password");
+		
+		
 	}
 	
 	
@@ -28,6 +46,11 @@ public class LifecycleServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		PrintWriter out = resp.getWriter();
 		out.println(this.getClass().getSimpleName()+" doPost method");
+	}
+	
+	@Override
+	public void destroy() {
+		
 	}
 
 }
--- 04_wcd/src/main/java/com/seed/servlet/LifecycleServlet.java
+++ 04_wcd/src/main/java/com/seed/servlet/LifecycleServlet.java
@@ -2,19 +2,26 @@ package com.seed.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
 
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.seed.util.ConnectionUtil;
+
 /**
  * Servlet implementation class LifecycleServlet
  */
-@WebServlet("/lifecycle")
+//@WebServlet("/lifecycle")
 public class LifecycleServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	private Connection con;
 
 	public LifecycleServlet() {
 		System.out.println(this.getClass().getSimpleName() + " construtor called");
@@ -23,6 +30,28 @@ public class LifecycleServlet extends HttpServlet {
 	@Override
 	public void init() throws ServletException {
 		System.out.println(this.getClass().getSimpleName() + " init method called");
+		
+		//read init  param
+		String a = this.getInitParameter("a");
+		ServletConfig config = this.getServletConfig();
+		String a1 = config.getInitParameter("a");
+		System.out.println("a= "+a+"    a1= "+a1);
+		
+		//read context param
+		ServletContext context = this.getServletContext();
+		String username = context.getInitParameter("db.username");
+		String password = context.getInitParameter("db.password");
+		String drivername = context.getInitParameter("db.drivername");
+		String url = context.getInitParameter("db.url");
+		System.out.println("username= "+username+"\ndrivername="+drivername+"\nurl="+url+"    \npassword"+password);
+		
+		
+		
+		//create the connection
+		con = ConnectionUtil.getConnection(drivername, url, username, password);
+		context.setAttribute("connection", con);
+		
+		
 	}
 
 	@Override
@@ -30,6 +59,7 @@ public class LifecycleServlet extends HttpServlet {
 		System.out.println(this.getClass().getSimpleName() + " doGet called");
 		PrintWriter out = resp.getWriter();
 		out.println(this.getClass().getSimpleName() + " doGet method");
+		req.setAttribute("connection", con);
 	}
 
 	@Override
